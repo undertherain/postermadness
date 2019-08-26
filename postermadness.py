936ae46f86407c208c9e4e018e47acf1d1bd9f76
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-08-26 17:27:52 +0900

    check if impressive is installed

--- postermadness.py
+++ postermadness.py
@@ -1,10 +1,29 @@
 from pathlib import Path
 import os
+import subprocess
 
 path_source = "source"
-duration=70
+duration = 70
 
+
+def clear():
+    # check and make call for specific operating system 
+    os.system('clear' if os.name =='posix' else 'cls')
+
+
+def check_env():
+    try:
+        subprocess.run(["impressive", "--version"])
+    except FileNotFoundError:
+        print("impressive is not installed")
+        exit(-1)
+
+check_env()
 for filename in sorted(Path(path_source).glob('**/*.pdf')):
+    clear()
     print("up next:", filename.name)
-
-    os.system(f"impressive -a {duration} --autoquit --duration {duration} --time-display --fontsize 28 --font /usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Regular.ttf {filename}")
+    input("Press Enter to start")
+    command = f"impressive -a {duration} --autoquit --duration {duration}"
+    command += f" --time-display --fontsize 28"
+    command += f" --font /usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Regular.ttf \"{filename}\""
+    os.system(command)
