d10767c94ad213a9994d0d7f7d8626a6273a81db
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-08-26 17:32:25 +0900

    remove bash version

--- postermadness.py
+++ postermadness.py
@@ -7,8 +7,7 @@ duration = 70
 
 
 def clear():
-    # check and make call for specific operating system 
-    subprocess.run(['clear'] if os.name =='posix' else 'cls')
+    subprocess.run(['clear'] if os.name == 'posix' else 'cls')
 
 
 def check_env():
@@ -18,12 +17,19 @@ def check_env():
         print("impressive is not installed")
         exit(-1)
 
-check_env()
-for filename in sorted(Path(path_source).glob('**/*.pdf')):
-    clear()
-    print("up next:", filename.name)
-    input("Press Enter to start")
-    command = f"impressive -a {duration} --autoquit --duration {duration}"
-    command += f" --time-display --fontsize 28"
-    command += f" --font /usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Regular.ttf \"{filename}\""
-    os.system(command)
+
+def main():
+    check_env()
+
+    for filename in sorted(Path(path_source).glob('**/*.pdf')):
+        clear()
+        print("up next:", filename.name)
+        input("Press Enter to start")
+        command = f"impressive -a {duration} --autoquit --duration {duration}"
+        command += f" --time-display --fontsize 28"
+        command += f" --font /usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Regular.ttf \"{filename}\""
+        os.system(command)
+
+
+if __name__ == "__main__":
+    main()
